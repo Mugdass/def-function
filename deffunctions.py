742de22e65688cab35698bea27cf852601188a06
Mugdass <84940755+Mugdass@users.noreply.github.com>
2021-06-07 13:53:28 -0700

    Add files via upload

---
+++ deffunctions.py
@@ -0,0 +1,12 @@
+print("Hey you know that")
+
+def fun(drink1,drink2):
+   print(drink1 + " is good for your body!")
+   print(drink2 + " is very not good for you!")
+   
+
+fun("water","soda")
+ 
+
+
+   
